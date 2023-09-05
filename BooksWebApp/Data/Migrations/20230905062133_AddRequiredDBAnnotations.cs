7ff675035c9f73230bec3695e8e87d0849997128
Priscilla Arinze <priscillaarinze@gmail.com>
2023-09-05 01:24:01 -0500

    Updated database field annotations to be required/non-nullable

---
+++ BooksWebApp/Data/Migrations/20230905062133_AddRequiredDBAnnotations.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BooksWebApp.Data.Migrations
+{
+    public partial class AddRequiredDBAnnotations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- BooksWebApp/Models/Book.cs
+++ BooksWebApp/Models/Book.cs
@@ -1,10 +1,18 @@
-﻿namespace BooksWebApp.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BooksWebApp.Models
 {
     public class Book
     {
+        [Key]
         public int Id { get; set; }
+        
+        [Required]
         public string Title { get; set; }
+
+        [Required]
         public string Author { get; set; }
+
         public string Genre { get; set; }
 
         public Book()
