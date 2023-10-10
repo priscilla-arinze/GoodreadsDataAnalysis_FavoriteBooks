213d7f116532939fd1d3b5a0db927aff16977b20
Priscilla Arinze <priscillaarinze@gmail.com>
2023-10-10 02:18:16 -0500

    -Renamed project -Lots of refactoring in Razor views -Addition of Goodreads Data Analysis controller, view, and header nav item -General updates to dependencies and metadata after rebuild

---
+++ GoodreadsDataAnalysisProject/Controllers/GoodreadsAnalysisController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoodreadsBookAnalysis.Controllers
+{
+    public class GoodreadsAnalysisController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
